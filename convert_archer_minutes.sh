4af7ea7248966ce20a795afa2543e0915dd913a5
Jeremy Osborne <jeremywosborne@gmail.com>
2016-05-07 19:19:03 -0700

    Case insensitive for flip camera.

--- convert_archer_minutes.sh
+++ convert_archer_minutes.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-for f in *.mp4; do
-    ffmpeg -i $f -c:v libx264 -preset slow -crf 23 -c:a copy -threads 0 -pix_fmt yuv420p `stat -f %Sm -t %Y%m%d_%H%M%S $f`.mkv; 
+for f in *.[Mm][Pp]4; do
+    ffmpeg -i $f -c:v libx264 -preset slow -crf 23 -c:a copy -threads 0 -pix_fmt yuv420p `stat -f %Sm -t %Y%m%d_%H%M%S $f`.mkv;
 done
